43a1e1480ac31f26980bd84e4b3ffff576abff89
Prajin Ranjit <prajinranjit5@gmail.com>
2024-05-01 21:32:02 +0545

    added responsive login and registration

--- src/app/login-page/LoginPage.jsx
+++ src/app/login-page/LoginPage.jsx
@@ -7,18 +7,18 @@ const LoginPage = () => {
 
   return (
     <section className="h-screen bg-bg_shapes bg-cover w-full relative flex justify-end overflow-hidden">
-      <div className="flex flex-col gap-4 absolute top-52 left-[20%]">
-        <h1 className="text-6xl font-bold text-background">Welcome Page</h1>
+      <div className="flex flex-col gap-4 absolute top-56 lg:top-64 left-[8%] xl:left-[15%]">
+        <h1 className="text-4xl lg:text-6xl font-bold text-background">Welcome Page</h1>
         <div className="relative">
-          <span className={`absolute text-2xl font-medium text-background transition-all duration-700 ease-linear ${registrationState ? 'opacity-0 translate-y-full' : 'delay-300 opacity-100 translate-y-0'}`}>
-            Sign in to continue access
-          </span>
-          <span className={`absolute text-2xl font-medium text-background transition-all duration-700 ease-linear ${registrationState ? 'delay-300 opacity-100 translate-y-0' : 'opacity-0 translate-y-full'}`}>
-            Register to access all the features of our service.
-          </span>
+          <p className={`absolute text-lg lg:text-2xl font-medium text-background transition-all duration-700 ease-linear ${registrationState ? 'opacity-0 translate-y-full' : 'delay-300 opacity-100 translate-y-0'}`}>
+            <span className="text-primary">Sign in</span> to continue access
+          </p>
+          <p className={`absolute text-lg lg:text-2xl font-medium text-background transition-all duration-700 ease-linear ${registrationState ? 'delay-300 opacity-100 translate-y-0' : 'opacity-0 translate-y-full'}`}>
+            <span className="text-primary">Register</span> to access all the features of our service.
+          </p>
         </div>
       </div>
-      <section className="w-[500px] h-full bg-bg_wave bg-no-repeat bg-contain bg-background shadow-nav text-text relative">
+      <section className="w-full md:w-[400px] lg:w-[450px] xl:w-[500px] h-full bg-bg_wave bg-no-repeat bg-contain bg-background shadow-nav text-text relative">
         <SignIn
           registrationState={registrationState}
           setRegistrationState={setRegistrationState}
--- src/app/login-page/SignIn.jsx
+++ src/app/login-page/SignIn.jsx
@@ -5,16 +5,16 @@ import Checkbox from "../../components/ui/forms/Checkbox";
 
 const SignIn = ({registrationState, setRegistrationState}) => {
   return (
-    <div className={`w-full h-full px-10 absolute right-0 flex flex-col items-center justify-center transition-all duration-700 ease-in-out ${registrationState ? 'opacity-0 translate-x-full' : 'delay-300 translate-x-0 opacity-100'}`}>
+    <div className={`w-full h-full px-3 sm:px-6 lg:px-10 absolute right-0 flex flex-col items-center justify-center transition-all duration-700 ease-in-out ${registrationState ? 'opacity-0 translate-x-full' : 'delay-300 translate-x-0 opacity-100'}`}>
       <h2 className="text-4xl font-bold text-black border-b-4 border-primary w-fit pb-1 text-primary">
         LOGIN
       </h2>
-      <span className="mt-3 mb-4">
+      <span className="mt-3 mb-4 text-center">
         Don't have an account yet?{" "}
         <button className="font-medium text-primary" onClick={() => setRegistrationState(true)}>Sign Up</button>
       </span>
       <form action="" className="w-full">
-        <Input id="email" label="Email" placeholder="email" type="email" />
+        <Input id="email" label="Email" placeholder="email sign" type="email" />
         <Input
           id="password"
           label="Password"
