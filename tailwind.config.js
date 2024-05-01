6497fcc465c36b44998d394b29f4718422414e71
Prajin Ranjit <prajinranjit5@gmail.com>
2024-05-01 12:10:53 +0545

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgba(var(--primary))",
+
+        text: "rgba(var(--text))",
+        background: "rgba(var(--background))",
+      },
+      screens: {
+        xs: "495px",
+        md2: "865px",
+      },
+      boxShadow: {
+        nav: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
